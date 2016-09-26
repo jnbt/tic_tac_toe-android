9dba55db3eadd7e820d5e8d7e0e795279fbc863b
Jonas Thiel <jt@neopoly.de>
2016-09-26 12:38:48 +0200

    Add main TODO to MainActivity

--- app/src/main/java/com/neopoly/tictactoe/MainActivity.java
+++ app/src/main/java/com/neopoly/tictactoe/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO Develop a Tic Tac Toe game here
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
